9e955743489b00baab9046559694e0c356ae35eb
Garmani98 <115382020+Garmani98@users.noreply.github.com>
2022-10-08 21:20:48 -0500

    Segundo Commit desde GitHub Online

--- hello.c
+++ hello.c
@@ -2,6 +2,10 @@
 Este es un comentario  puesto desde el editor en linea del GitHub
 */
 
+/* 
+ Este es el segundo comentario puesto en editor GitHub Online
+*/
+
 #include <stdio.h>
 
 int main(void){
